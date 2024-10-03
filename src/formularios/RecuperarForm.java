47c365fa2a869f865512ba83003624d98de4e770
amezquita <angel_amezquita02@outlook.com>
2024-10-02 21:58:53 -0600

    Modificacion de diseño Recuperar contraseña

--- src/formularios/RecuperarForm.java
+++ src/formularios/RecuperarForm.java
@@ -68,27 +68,32 @@ public class RecuperarForm extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jTextUser2 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jTextRecordar = new javax.swing.JTextField();
         jbtnvolver2 = new javax.swing.JButton();
         jbtnrecuperar = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jTextRecordar1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(0, 204, 204));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(jLabel1.getFont().deriveFont((float)24));
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Recuperar Contraseña");
-
-        jLabel3.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Yu Gothic UI Semilight", 0, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 204, 204));
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/userLabel.png"))); // NOI18N
         jLabel3.setText("Usuario:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 200, -1, -1));
 
         jTextUser2.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
         jTextUser2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -98,11 +103,13 @@ public class RecuperarForm extends javax.swing.JFrame {
                 jTextUser2ActionPerformed(evt);
             }
         });
+        jPanel1.add(jTextUser2, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 220, 234, -1));
 
-        jLabel4.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Yu Gothic UI Semilight", 0, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 204, 204));
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/key.png"))); // NOI18N
-        jLabel4.setText("Mostrar Clave:");
+        jLabel4.setText("Telefono");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 270, -1, -1));
 
         jTextRecordar.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
         jTextRecordar.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -112,16 +119,17 @@ public class RecuperarForm extends javax.swing.JFrame {
                 jTextRecordarActionPerformed(evt);
             }
         });
+        jPanel1.add(jTextRecordar, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 290, 234, -1));
 
-        jbtnvolver2.setBackground(new java.awt.Color(204, 255, 255));
         jbtnvolver2.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
         jbtnvolver2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/previous.png"))); // NOI18N
-        jbtnvolver2.setText("Volver");
+        jbtnvolver2.setBorder(null);
         jbtnvolver2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbtnvolver2ActionPerformed(evt);
             }
         });
+        jPanel1.add(jbtnvolver2, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 430, 30, 30));
 
         jbtnrecuperar.setBackground(new java.awt.Color(204, 255, 255));
         jbtnrecuperar.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
@@ -131,52 +139,53 @@ public class RecuperarForm extends javax.swing.JFrame {
                 jbtnrecuperarActionPerformed(evt);
             }
         });
+        jPanel1.add(jbtnrecuperar, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 430, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(91, 91, 91)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel4)
-                    .addComponent(jTextUser2)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jTextRecordar))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(jbtnvolver2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
-                .addComponent(jbtnrecuperar)
-                .addGap(67, 67, 67))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(jLabel1)
-                .addGap(24, 24, 24)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextUser2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextRecordar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbtnvolver2)
-                    .addComponent(jbtnrecuperar))
-                .addContainerGap(33, Short.MAX_VALUE))
-        );
+        jLabel6.setFont(new java.awt.Font("Yu Gothic UI Semilight", 0, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(0, 204, 204));
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/key.png"))); // NOI18N
+        jLabel6.setText("Correo Electronico");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 340, -1, -1));
+
+        jTextRecordar1.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
+        jTextRecordar1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextRecordar1.setName("jTextUsuario"); // NOI18N
+        jTextRecordar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextRecordar1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jTextRecordar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 360, 234, -1));
+
+        jLabel2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("HResources");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 40, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("GESTIONA EL FUTURO DE TU EQUIPO, HOY");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 80, -1, -1));
+
+        jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Sin título(2).png"))); // NOI18N
+        jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 560, 500));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Sin título(1).png"))); // NOI18N
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 0, 730, 60));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/candado.png"))); // NOI18N
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 120, -1, 80));
+
+        jLabel9.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(0, 204, 204));
+        jLabel9.setText("RECUPERAR CONTRASEÑA ");
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 80, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -216,6 +225,10 @@ public class RecuperarForm extends javax.swing.JFrame {
 
     }//GEN-LAST:event_jbtnrecuperarActionPerformed
 
+    private void jTextRecordar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextRecordar1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextRecordar1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -252,11 +265,18 @@ public class RecuperarForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextRecordar;
+    private javax.swing.JTextField jTextRecordar1;
     private javax.swing.JTextField jTextUser2;
     private javax.swing.JButton jbtnrecuperar;
     private javax.swing.JButton jbtnvolver2;
